3ffc6973791bce816af838b19aee9a8c56dc1060
michal842 <38220960+michal842@users.noreply.github.com>
2020-12-22 18:22:03 +0200

    Update Shapes.py

--- Shapes.py
+++ Shapes.py
@@ -1,33 +1,30 @@
-import random
-import time
-from ControlServo import Angles
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import RPi.GPIO as GPIO
 
-angles = Angles.instance()
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(2, GPIO.OUT)
+Request=None
 
-class Square:
-    def __init__(self, points):
-        self.points = points
+class RequestHandler_httpd(BaseHTTPRequestHandler):
+	def do_GET(self):
+		global Request
+		messagetosend=bytes('Hello World!', "utf")
+		self.send_response(200)
+		self.send_header('Content-Type', 'text/plain')
+		self.send_header('Content-Length', len(messagetosend))
+		self.end_headers()
+		self.wfile.write(messagetosend)
+		Request=self.requestline
+		Request=Request[5: int(len(Request)-9)]
+		print(Request)
+		if Request == 'on':
+			GPIO.output(2, True)
+		if Request == 'off':
+			GPIO.output(2, False)
+		return
 
-    def run(self, duration=10, travel_time=0.5, delay=0.5):
-        start = time.time()
-        while time.time() - start < duration:
-            new_point = self.points[random.randrange(len(self.points))]
-            angles.move(new_point, travel_time)
-            time.sleep(delay)
-
-
-class Star:
-    def __init__(self, center, edges):
-        self.centet = center
-        self.edges = edges
-
-
-
-
-
-class Circle:
-    pass
-
-
-class Trail: #might become infinity
-    pass
+server_address_httpd=('192.168.1.55',8080)
+httpd=HTTPServer(server_address_httpd, RequestHandler_httpd)
+print('Starting server:')
+httpd.serve_forever()
+GPIO.cleanup()
